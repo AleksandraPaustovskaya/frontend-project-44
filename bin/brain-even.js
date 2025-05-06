73670083c12a1c5888e7689202ecbaea85143c71
Aleksandra <paustovskayasasha@gmail.com>
2025-05-06 22:29:08 +0300

    add name of func

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,20 +3,22 @@ import readline from 'readline-sync'
 import name from '../src/cli.js'
 
 const playEven = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".')
-
   let counterAnswers = 0
   let userName = name()
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
   for (let i = 0; i < 3; i++) {
     let number = Math.floor(Math.random() * 1000 % 1000)
     console.log(`Question:${number}`)
-    const evening = readline.question('Your answe: ') === 'yes' ? true : false
-    if (number % 2 === 0 && evening) {
+    const evening = readline.question('Your answe: ') //=== 'yes' ? true : false
+    // console.log(evening);
+    
+    if (number % 2 === 0 && evening === 'yes') {
       console.log('Correct!')
       counterAnswers++
     }
-    else if (number % 2 === 1 && !evening) {
+    else if (number % 2 === 1 && evening === 'no') {
       console.log('Correct!')
       counterAnswers++
     }
@@ -29,6 +31,7 @@ const playEven = () => {
     }
   }
 }
-playEven();
+
+playEven()
 
 export default playEven
--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,8 @@
 import readlineSync from 'readline-sync'
 
 const isName = () => {
+  console.log('Welcome to the Brain Games!')
+
   console.log('May I have your name?')
 
   const name = readlineSync.question('Your answer: ')
