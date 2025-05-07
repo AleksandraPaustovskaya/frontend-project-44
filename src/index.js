a26f0a49a90614848f982006e292fb6232dadced
Aleksandra <paustovskayasasha@gmail.com>
2025-05-07 23:21:20 +0300

    the general logical of the game has been moved to index.js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import readline from 'readline-sync'
+import name from '../src/cli.js'
+
+const randomFrom1to99 = () => {
+    const rand = Math.floor(Math.random() * 100)
+    return rand
+  }
+
+const playGame = (rool, play) => {
+    const userName = name()
+    let i = 3;
+
+    console.log(rool)
+    while (i > 0) {
+    const f = play()
+    const answer = Number(readline.question('Your answer: '))
+
+    console.log(f)
+
+    f === answer ? console.log('Correct!') : i = 0;
+      
+      i--
+    }
+    console.log(i === 0 ? `Congratulations, ${userName}!` : `Let's try again, ${userName}!`) 
+    
+
+}
+
+export {randomFrom1to99, playGame}
