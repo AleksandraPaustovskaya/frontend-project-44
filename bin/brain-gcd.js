d8d75947062a20d43d92097df100b9c7e805700e
Aleksandra <paustovskayasasha@gmail.com>
2025-05-09 16:33:15 +0300

    добавила расширение

---
+++ bin/brain-gcd.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import playGame from '../src/index.js'
+const gcdPlayRools = 'Find the greatest common divisor of given numbers.'
+
+const playGCD = (random) => {
+    let a = random(3)
+    let b = random()
+
+    let counter = 1;
+    let gcd = b;
+
+
+    while (b/counter >= 1) {
+        gcd = b/counter
+        if (Math.floor(b/gcd)*gcd === b) {
+            if (Math.floor(a/(gcd))*gcd === a) {
+                return [`${a} ${b}`, `${gcd}`]
+            }
+        }
+        counter++
+    }
+
+    return [[`${a}`, `${b}`], `${gcd}`]
+}
+
+
+playGame(gcdPlayRools, playGCD)
