9907900a62819585f11c209669f69e5a341ff338
Aleksandra <paustovskayasasha@gmail.com>
2025-05-09 17:11:50 +0300

    new game, progression, lets test from first time!:)

---
+++ bin/brain-progression.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import playGame from '../src/index.js'
+const progressionPlayRools = 'What file is missing in the progression'
+
+const playProgression = (random) => {
+  let quantityOfProgression = 10
+  let stepOfProgression = random(1)
+  let positionStealth = Number(random(1))
+  const progressionCollect = [random(1)]
+
+  for (let i = 1; i < quantityOfProgression; i++) {
+    let nextElement = progressionCollect[i - 1] + stepOfProgression
+    progressionCollect.push(nextElement)
+  }
+  let answer = progressionCollect[positionStealth]
+  progressionCollect[positionStealth] = '..'
+
+  return [progressionCollect, answer]
+}
+
+playGame(progressionPlayRools, playProgression)
